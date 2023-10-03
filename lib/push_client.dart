a7fbdacfc4afb77714ae39b00db3403c337ccd7d
Miguel Albanez <malbanez@finmarkets.cl>
2023-10-03 12:05:57 -0300

    first commit

---
+++ lib/push_client.dart
@@ -0,0 +1,6 @@
+library push_client;
+
+export 'src/client.dart' show FayeClient, FayeClientState;
+export 'src/subscription.dart';
+export 'src/extensible.dart' show MessageHandler;
+export 'src/error.dart';
